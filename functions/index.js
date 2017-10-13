a571a5a24fafb2e7a67b40de2e03da48d3587530
unknown <jobizzness@github.io>
2017-10-13 15:20:00 +0100

    register new user

--- functions/index.js
+++ functions/index.js
@@ -4,8 +4,6 @@ const fetch = require('node-fetch');
 const url = require('url');
 const app = express();
 
-const pwaShell = require('./pwashell');
-
 /**
  * generateUrl() - Piece together request parts to form FQDN URL
  * @param {Object} request 
@@ -47,50 +45,50 @@ const checkForBots = (userAgent) => {
 // configured rewrites (see "Hosting Priorities" https://firebase.google.com/docs/hosting/url-redirects-rewrites)
 // 
 // The trick is on L66, pwaShell(): You must update that file! Open for explainer.
-app.get('*', (req, res) => {
+// app.get('*', (req, res) => {
 
-  // What say you bot tester?
-  const botResult = checkForBots(req.headers['user-agent']);
+//   // What say you bot tester?
+//   const botResult = checkForBots(req.headers['user-agent']);
 
-  if (botResult) {
+//   if (botResult) {
 
-    // Get me the url all nice
-    const targetUrl = generateUrl(req);
+//     // Get me the url all nice
+//     const targetUrl = generateUrl(req);
 
-    // Did you read the README? You should have set functions.config().botrender.server
-    // to where ever you deployed https://github.com/samuelli/bot-render on AppEngine
-    fetch(`${functions.config().botrender.server}?url=${targetUrl}`)
-      .then(function(res) {
-        return res.text();
-      }).then(function(body) {
+//     // Did you read the README? You should have set functions.config().botrender.server
+//     // to where ever you deployed https://github.com/samuelli/bot-render on AppEngine
+//     fetch(`${functions.config().botrender.server}?url=${targetUrl}`)
+//       .then(function(res) {
+//         return res.text();
+//       }).then(function(body) {
 
-        // This is only going to return the HEAD of the document, nothing else 
-        // This is debatably AWESOME (save some bytes, give the bots only what they want)
-        // See https://github.com/samuelli/bot-render/blob/master/renderer.js#L37
+//         // This is only going to return the HEAD of the document, nothing else 
+//         // This is debatably AWESOME (save some bytes, give the bots only what they want)
+//         // See https://github.com/samuelli/bot-render/blob/master/renderer.js#L37
 
-        // We set Vary because we only want to cache this result for the bots
-        // which we know based on the user-agent. Vary is very useful.
-        // Reading about Vary header: 
-        //  https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Vary
-        //  https://www.fastly.com/blog/best-practices-using-vary-header/
-        res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
-        res.set('Vary', 'User-Agent');
+//         // We set Vary because we only want to cache this result for the bots
+//         // which we know based on the user-agent. Vary is very useful.
+//         // Reading about Vary header: 
+//         //  https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Vary
+//         //  https://www.fastly.com/blog/best-practices-using-vary-header/
+//         res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+//         res.set('Vary', 'User-Agent');
         
-        res.send(body.toString());
+//         res.send(body.toString());
       
-    });
+//     });
 
-  } else {
+//   } else {
 
-    // 1. Umm, Justin, why not just point to index.html? 
-    // 2. Umm, Justin, why not just fetch() index.html from the domain?
-    // 
-    // Valid things to ask internet peoples
-    // 1. function doesn't know about the public hosting as far as I can tell (docs don't offer opinion/example)
-    // 2. Could fetch and return...but I found copy+paste the index.html PWA shell into file returns faster
-    res.send(pwaShell());
-  }
+//     // 1. Umm, Justin, why not just point to index.html? 
+//     // 2. Umm, Justin, why not just fetch() index.html from the domain?
+//     // 
+//     // Valid things to ask internet peoples
+//     // 1. function doesn't know about the public hosting as far as I can tell (docs don't offer opinion/example)
+//     // 2. Could fetch and return...but I found copy+paste the index.html PWA shell into file returns faster
+//     res.send(pwaShell());
+//   }
   
-});
+// });
 
-exports.app = functions.https.onRequest(app);
+// exports.app = functions.https.onRequest(app);
